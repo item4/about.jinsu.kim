c7c99b449f38c5446832570b96e04751bcec049f
Kim, Jinsu <item4hun@gmail.com>
2022-06-10 19:16:33 +0900

    Change rule of part time jobs

--- src/pages/parttime.tsx
+++ src/pages/parttime.tsx
@@ -55,6 +55,16 @@ const PartTimeJobPage: React.FC<PartTimeJobPageProps> = ({
         </p>
       </PartTimeJobSection>
       <PartTimeJobSection>
+        <SectionTitle>받지 않는 업무</SectionTitle>
+        <ul>
+          <li>대리시험</li>
+          <li>
+            과제물의 결과물만 요구하는 경우 (과제물은 과외의 경우에만 같이
+            풀어드립니다)
+          </li>
+        </ul>
+      </PartTimeJobSection>
+      <PartTimeJobSection>
         <SectionTitle>가격 산정</SectionTitle>
         <p>
           기본적으로 업무 난이도에 따라 30,000 KRW에서 50,000 KRW 정도의 시급을
@@ -81,144 +91,6 @@ const PartTimeJobPage: React.FC<PartTimeJobPageProps> = ({
         </ul>
       </PartTimeJobSection>
       <PartTimeJobSection>
-        <SectionTitle>단골 목록</SectionTitle>
-        <ul>
-          <li>익명(법인) - express/flutter 기반 서비스 유지보수</li>
-        </ul>
-      </PartTimeJobSection>
-      <PartTimeJobSection>
-        <SectionTitle>작업 완료 목록</SectionTitle>
-        <ul>
-          <li>
-            50,000 KRW - disjukr님 (
-            <a
-              href='https://github.com/disjukr/just-news/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              disjukr/just-news
-            </a>
-            ,{' '}
-            <a
-              href='https://github.com/disjukr/just-news/commit/63705b878c621acaa8c071deae107f611ba059c9'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              [commit log]
-            </a>
-            , GitHub Actions 도입)
-          </li>
-          <li>
-            60,000 KRW - 익명 (aiohttp를 이용한 특정 사이트의 공지사항 수집)
-          </li>
-          <li>15,000 KRW - 익명 (정규식을 이용한 자료정리)</li>
-          <li>
-            50,000 KRW - disjukr님 (
-            <a
-              href='https://github.com/disjukr/just-news/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              disjukr/just-news
-            </a>
-            ,{' '}
-            <a
-              href='https://github.com/disjukr/just-news/commit/d62fb916d6fcb7a507d1adaf7e5116296bfcd9d2'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              [commit log]
-            </a>
-            , GitHub Actions 개선)
-          </li>
-          <li>
-            18,000 KRW - 익명 (docker-compose를 사용한 백엔드 서버 환경 조성)
-          </li>
-          <li>
-            100,000 KRW - 익명 (flask-sqlalchemy를 사용한 간단한 웹사이트 개발)
-          </li>
-          <li>
-            50,000 KRW - disjukr님 (
-            <a
-              href='https://github.com/disjukr/just-news/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              disjukr/just-news
-            </a>
-            ,{' '}
-            <a
-              href='https://github.com/disjukr/just-news/commit/5e082dc64fc6899dacb831cc1d11a92d33fab3ef'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              [commit log]
-            </a>
-            , GitHub Actions에서 puppeteer 사용하는 문제 개선)
-          </li>
-          <li>50,000 KRW - 익명 (TypeScript로 작성된 코드의 검색 기능 개선)</li>
-          <li>
-            50,000 KRW - hongminhee님 (
-            <a
-              href='https://github.com/dahlia/seonbi'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              dahlia/seonbi
-            </a>
-            ,{' '}
-            <a
-              href='https://github.com/dahlia/seonbi/pull/4'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              [PR#4]
-            </a>
-            , Elm으로 작성된 웹페이지 오작동 수정)
-          </li>
-          <li>130,000 KRW - 익명 (GatsbyJS로 정적 웹사이트 개발)</li>
-          <li>100,000 KRW- 익명 (Python requests기반으로 뉴스기사 크롤링)</li>
-          <li>
-            200,000 KRW - 익명 (Python + Flask + uWSGI + nginx + Docker 세팅법
-            안내)
-          </li>
-          <li>
-            20,000 KRW - 익명 (웹 페이지가 브라우저간 다르게 랜더링 되는 문제
-            해소)
-          </li>
-          <li>
-            300,000 KRW - comet님 (
-            <a
-              href='https://github.com/microsoft/TypeScript-Website'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              microsoft/TypeScript-Website
-            </a>
-            ,{' '}
-            <a
-              href='https://github.com/microsoft/TypeScript-Website/pull/266'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              [PR#266]
-            </a>
-            , Gatsby용 TypeScript 플러그인 교체)
-          </li>
-          <li>100,000 KRW - 익명 (doc 파일을 markdown 문법으로 변환작업)</li>
-          <li>
-            200,000 KRW - 타로님 (Rust로 특정 API 상태에 따라 Slack에 메시지
-            보내고, Slack 특정 명령어에 반응해서 API 상태 가져오는 Slack Bot
-            작성)
-          </li>
-          <li>
-            40,000 KRW - lazygyu님 (TypeScript로 근태 메시지 파싱 함수 작성)
-          </li>
-          <li>40,000 KRW - 익명 (Git/GitHub 기초적인 사용법 과외)</li>
-          <li>60,000 KRW - 익명 (Slack API 활용한 자료 Sync)</li>
-        </ul>
-      </PartTimeJobSection>
-      <PartTimeJobSection>
         <SectionTitle>정책 안내</SectionTitle>
         <ol>
           <li>
@@ -229,6 +101,15 @@ const PartTimeJobPage: React.FC<PartTimeJobPageProps> = ({
           <li>
             오픈소스 작업을 선호하지만 오픈소스가 아니어도 상관 없습니다. 보안
             유지가 필요한 경우 미리 말씀해주세요.
+          </li>
+          <li>
+            대리시험 응시 내지는 시험 도중 부정행위에 도움을 달라는 의뢰는 받지
+            않습니다.
+          </li>
+          <li>
+            과제물을 도와드리는 것은 어디까지나 공부를 하는 분에게 도움을 드리기
+            위한 것이지, 학습할 의향 없이 답안만 요구하는 경우에는 일절 도움을
+            드리지 않습니다.
           </li>
         </ol>
       </PartTimeJobSection>
