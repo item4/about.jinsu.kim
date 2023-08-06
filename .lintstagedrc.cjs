b402b908147b3bbbf1c59942ef0b9d03ce316561
Kim Jinsu <item4hun@gmail.com>
2023-08-06 14:01:22 +0900

    의존성 업데이트

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,9 +3,9 @@ const path = require('node:path');
 const srcDir = path.join(process.cwd(), 'src');
 
 const make_next_lint_command = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(srcDir, f)).join(' --file ')}`;
+  `yarn exec next lint --fix --file ${filenames.map((f) => path.relative(srcDir, f)).join(' --file ')}`;
 
 module.exports = {
-  '*.{ts,tsx}': [make_next_lint_command, 'prettier --write', 'git add'],
-  '*.{css,cjs,mjs,json,md,yml,yaml}': ['prettier --write', 'git add'],
+  '*.{ts,tsx}': [make_next_lint_command, 'yarn exec prettier --write', 'git add'],
+  '*.{css,cjs,mjs,json,md,yml,yaml}': ['yarn exec prettier --write', 'git add'],
 };
