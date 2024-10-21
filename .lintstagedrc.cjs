73c8f1a36748f8edd7ac468164178e182399dc97
Kim Jinsu <item4hun@gmail.com>
2024-10-22 07:56:15 +0900

    Next.js v15

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,9 +1,9 @@
 const path = require('node:path');
 
-const srcDir = path.join(process.cwd(), 'src');
+const pwd = process.cwd();
 
 const make_next_lint_command = (filenames) =>
-  `yarn exec next lint --fix --file ${filenames.map((f) => path.relative(srcDir, f)).join(' --file ')}`;
+  `yarn exec next lint --fix --file ${filenames.map((f) => path.relative(pwd, f)).join(' --file ')}`;
 
 module.exports = {
   '*.{ts,tsx}': [make_next_lint_command, 'yarn exec prettier --write', 'git add'],
