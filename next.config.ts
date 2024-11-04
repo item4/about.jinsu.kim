73c4643c956f62b76b6e850acb3b5d2740404c9a
Kim Jinsu <item4hun@gmail.com>
2024-11-04 17:17:44 +0900

    `skipTrailingSlashRedirect` 활성화

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   output: 'export',
   cleanDistDir: true,
   productionBrowserSourceMaps: true,
