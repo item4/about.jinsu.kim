9fa3bef9e606308c48b37d79fff3e9bd6bccc331
Kim Jinsu <item4hun@gmail.com>
2025-02-25 09:19:05 +0900

    tsconfig/lintstagedrc 설정 블로그와 가급적 유사하게 변경

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -6,6 +6,6 @@ const make_next_lint_command = (filenames) =>
   `yarn exec next lint --fix --file ${filenames.map((f) => path.relative(pwd, f)).join(' --file ')}`;
 
 module.exports = {
-  '*.{css,js,jsx,cjs,mjs,ts,tsx,json,jsonc}': [make_next_lint_command, 'yarn exec prettier --write', 'git add'],
+  '*.{js,jsx,cjs,mjs,ts,tsx,css,json,jsonc}': [make_next_lint_command, 'yarn exec prettier --write', 'git add'],
   '*.{md,yml,yaml}': ['yarn exec prettier --write', 'git add'],
 };
